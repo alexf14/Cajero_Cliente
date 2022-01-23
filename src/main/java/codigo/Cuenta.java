26f57b71a89430bd3bb22035b3bd774617b2ea22
alexf14 <alexf14f@gmail.com>
2022-01-23 17:03:40 +0100

    Esta limitado tanto el limite diario como el limite mensual

--- src/main/java/codigo/Cuenta.java
+++ src/main/java/codigo/Cuenta.java
@@ -8,6 +8,7 @@ public class Cuenta {
 
     Conector conector = new Conector();
     GuardarDatos g = new GuardarDatos();
+   
 
     public Cuenta() {
     }
@@ -58,43 +59,25 @@ public class Cuenta {
     }
 
     public void actualizarSaldo(float incremento, boolean operacion) {
+         Tarjeta tarjeta = new Tarjeta ();
+         
         String tipo;
+        boolean reintegro = false;
         String cuenta = "update cuenta set saldo = saldo + 0 where cuenta.numero = '" + this.getNum(g.getnTarjeta()) + "'";
 
-        String diario = "SELECT SUM(operacion.cantidad) from operacion where operacion.numero_cuenta = '" + this.getNum(g.getnTarjeta()) + "' "
-                + "and operacion.tipo = 'REINTEGRO' and year(time_stamp) = year(time_stamp) and month(time_stamp) = month(time_stamp) "
-                + "and day(time_stamp) = day(time_stamp)";
-
-        String mensual = "SELECT SUM(operacion.cantidad) from operacion where operacion.numero_cuenta = '" + this.getNum(g.getnTarjeta()) + "' "
-                + "and operacion.tipo = 'REINTEGRO' and year(time_stamp) = year(time_stamp) and month(time_stamp) = month(time_stamp) ";
-
         PreparedStatement ps = null;
-        PreparedStatement dr = null;
-        PreparedStatement ms = null;
         Connection con = conector.conectar();
-
-        boolean reintegro = false;
 
         if (operacion) {
             cuenta = "update cuenta set saldo = saldo + '" + incremento + "' where cuenta.numero = '" + this.getNum(g.getnTarjeta()) + "'";
         } else {
-            try {
-                dr = con.prepareStatement(diario);
-                ms = con.prepareStatement(mensual);
-                dr.executeQuery();
-                ms.executeQuery();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-            if (dr.next() && ms.next()) {
-                saldo = dr.getFloat(1);
-                if () {
-                    if (this.getSaldo(g.getnTarjeta()) - incremento > 0) {
-                        cuenta = "update cuenta set saldo = saldo - '" + incremento + "' where cuenta.numero = '" + this.getNum(g.getnTarjeta()) + "'";
-                    } else {
-                        reintegro = true;
-                    }
+            if(tarjeta.getLimiteDiario() > this.gastos(true) + incremento && tarjeta.getLimiteMensual() > this.gastos(false) + incremento){
+                if (this.getSaldo(g.getnTarjeta()) - incremento > 0) {
+                    reintegro = false;
+                    cuenta = "update cuenta set saldo = saldo - '" + incremento + "' where cuenta.numero = '" + this.getNum(g.getnTarjeta()) + "'";
                 }
+            }else{
+                reintegro = true;
             }
         }
 
@@ -114,6 +97,36 @@ public class Cuenta {
         }
 
         this.operacion(tipo, incremento);
+    }
+    
+    private float gastos (boolean tipoGasto){
+        String gasto;
+        float intGastos = 0;
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection con = conector.conectar();
+        
+        if(tipoGasto){
+            gasto = "SELECT SUM(operacion.cantidad) from operacion where operacion.numero_cuenta = '" + this.getNum(g.getnTarjeta()) + "' "
+                + "and operacion.tipo like 'REINTEGRO' and year(time_stamp) = year(time_stamp) and month(time_stamp) = month(time_stamp) "
+                + "and day(time_stamp) = day(time_stamp)";
+        }else{
+            gasto = "SELECT SUM(operacion.cantidad) from operacion where operacion.numero_cuenta = '" + this.getNum(g.getnTarjeta()) + "' "
+                + "and operacion.tipo like 'REINTEGRO' and year(time_stamp) = year(time_stamp) and month(time_stamp) = month(time_stamp) ";
+        }
+        
+        try {
+            ps = con.prepareStatement(gasto);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                intGastos = rs.getFloat(1);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return intGastos;
     }
 
     private void operacion(String tipo, float cantidad) {
--- src/main/java/codigo/Tarjeta.java
+++ src/main/java/codigo/Tarjeta.java
@@ -6,6 +6,53 @@ import java.sql.ResultSet;
 
 public class Tarjeta {
     Cuenta c = new Cuenta ();
+    Conector conector = new Conector();
+    GuardarDatos g = new GuardarDatos ();
+    
+    String limiteDiario;
+    String limiteMensual;
+    
+    public float getLimiteDiario (){
+        String diario = "select valor_limite from limite_tarjeta where tipo_limite = 'DIARIO' and numero_tarjeta = '"+g.getnTarjeta()+"'";
+        int limite = 0;
+        
+         PreparedStatement ps = null;
+         ResultSet rs = null;
+         Connection con = conector.conectar();
+        
+        try {
+            ps = con.prepareStatement(diario);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                limite = rs.getInt(1);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return limite;
+    }
+    
+    public float getLimiteMensual (){
+        String mensual = "select valor_limite from limite_tarjeta where tipo_limite = 'MENSUAL' and numero_tarjeta = '"+g.getnTarjeta()+"'";
+        int limite = 0;
+        
+         PreparedStatement ps = null;
+         ResultSet rs = null;
+         Connection con = conector.conectar();
+        
+        try {
+            ps = con.prepareStatement(mensual);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                limite = rs.getInt(1);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return limite;
+    }
     
     public boolean validar(String num, String cvs, String pin, String fCad){
         Conector conector = new Conector();
